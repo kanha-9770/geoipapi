4374aab84899a5c4d4706becacd6f07949928003
kanha-9770 <app3.nessco@gmail.com>
2024-10-22 11:57:48 +0530

    doing redeployment

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const geoip = require("geoip-country"); // Alternative package
-
+const geoip = require('geoip-lite');
 const app = express();
 const port = 8000;
 
@@ -11,29 +10,20 @@ app.get("/", (req, res) => {
 app.get("/geoip/:ip", async (req, res, next) => {
   try {
     const ip = req.params.ip;
-
-    // Basic IP validation (IPv4)
-    const ipRegex =
-      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-
-    if (!ipRegex.test(ip)) {
-      return res.status(400).json({ error: "Invalid IP address format" });
-    }
-
-    // GeoIP lookup with error handling
     const geo = geoip.lookup(ip);
-
     if (geo) {
-      return res.json({
+      res.json({
         ip: ip,
         country: geo.country,
+        region: geo.region,
+        city: geo.city,
+        ll: geo.ll, // latitude and longitude
+        timezone: geo.timezone || null,
       });
     } else {
-      console.error(`GeoIP lookup failed for IP: ${ip}`);
-      return res.status(404).json({ error: "GeoIP information not found" });
+      res.status(404).json({ error: "GeoIP information not found" });
     }
   } catch (err) {
-    console.error(err); // Log error
     next(err);
   }
 });
